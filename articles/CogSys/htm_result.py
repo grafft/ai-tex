de76f451b4d8800c41f50c19346622559b6e3c7e
grafft <sanekpanov@gmail.com>
2015-09-21 21:30:37 +0300

    [+] RITA fixes

--- articles/CogSys/htm_result.py
+++ articles/CogSys/htm_result.py
@@ -2,12 +2,12 @@ import matplotlib.pyplot as plt
 
 # the histogram of the data
 y = [90,86,78,75,72,66,57,53,50,48]
-y = [10,14,22,25,28,34,43,47,50,52]
+y = [100-x for x in y]
 plt.bar(range(1,11),y,width=0.5,align='center', color='#afafaf')
 
 
-plt.xlabel('Noise level')
-plt.ylabel('Error rate, %')
+plt.xlabel('Noise level', fontsize=20)
+plt.ylabel('Error rate, %', fontsize=20)
 #plt.title('Histogram of R-FSM precision')
 plt.axis([0, 11, 0, 70])
 plt.grid(True)
